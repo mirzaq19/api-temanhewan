4b4eda8e4dbd22fe885e52f1d01b94593e1a3bc6
M. Auliya Mirzaq Romdloni <mirzaqarjap@gmail.com>
2022-04-29 17:18:21 +0700

    feat: bind pet repository

--- src/app/Temanhewan/dependencies.php
+++ src/app/Temanhewan/dependencies.php
@@ -3,9 +3,12 @@
 use Illuminate\Contracts\Foundation\Application;
 
 use App\Temanhewan\Core\Domain\Repository\UserRepository;
+use App\Temanhewan\Core\Domain\Repository\PetRepository;
 
 use App\Temanhewan\Infrastructure\Repository\SqlUserRepository;
+use App\Temanhewan\Infrastructure\Repository\SqlPetRepository;
 
 /** @var Application $app */
 
 $app->bind(UserRepository::class, SqlUserRepository::class);
+$app->bind(PetRepository::class, SqlPetRepository::class);
