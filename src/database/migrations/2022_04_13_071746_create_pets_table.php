997383366826c5aade9e9f3b63b7597ac25fb982
M. Auliya Mirzaq Romdloni <mirzaqarjap@gmail.com>
2022-05-23 08:36:43 +0700

    fix: change petId column into primary key

--- src/database/migrations/2022_04_13_071746_create_pets_table.php
+++ src/database/migrations/2022_04_13_071746_create_pets_table.php
@@ -14,7 +14,7 @@ return new class extends Migration
     public function up()
     {
         Schema::create('pets', function (Blueprint $table) {
-            $table->uuid('id');
+            $table->uuid('id')->primary();
             $table->string('name');
             $table->string('profile_image');
             $table->text('description')->nullable();
