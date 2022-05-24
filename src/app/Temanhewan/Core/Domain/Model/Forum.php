efe06a5071c05a9b7a868cf74afb4be944e9eb09
M. Auliya Mirzaq Romdloni <mirzaqarjap@gmail.com>
2022-05-24 13:39:54 +0700

    feat: add forum model

---
+++ src/app/Temanhewan/Core/Domain/Model/Forum.php
@@ -0,0 +1,70 @@
+<?php
+
+namespace App\Temanhewan\Core\Domain\Model;
+
+class Forum
+{
+    private ForumId $id;
+    private string $slug;
+    private string $title;
+    private string $subtitle;
+    private string $content;
+
+    /**
+     * @param ForumId $id
+     * @param string $slug
+     * @param string $title
+     * @param string $subtitle
+     * @param string $content
+     */
+    public function __construct(ForumId $id, string $slug, string $title, string $subtitle, string $content)
+    {
+        $this->id = $id;
+        $this->slug = $slug;
+        $this->title = $title;
+        $this->subtitle = $subtitle;
+        $this->content = $content;
+    }
+
+    /**
+     * @return ForumId
+     */
+    public function getId(): ForumId
+    {
+        return $this->id;
+    }
+
+    /**
+     * @return string
+     */
+    public function getSlug(): string
+    {
+        return $this->slug;
+    }
+
+    /**
+     * @return string
+     */
+    public function getTitle(): string
+    {
+        return $this->title;
+    }
+
+    /**
+     * @return string
+     */
+    public function getSubtitle(): string
+    {
+        return $this->subtitle;
+    }
+
+    /**
+     * @return string
+     */
+    public function getContent(): string
+    {
+        return $this->content;
+    }
+
+
+}
---
+++ src/app/Temanhewan/Core/Domain/Model/ForumId.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Temanhewan\Core\Domain\Model;
+
+use InvalidArgumentException;
+use Ramsey\Uuid\Uuid;
+
+class ForumId
+{
+    private string $id;
+
+    public function __construct(string $id = null)
+    {
+        if (is_null($id)) {
+            $this->id = Uuid::uuid4()->toString();
+            return;
+        }
+
+        if (Uuid::isValid($id)) {
+            $this->id = $id;
+        } else {
+            throw new InvalidArgumentException("Invalid ForumId format.",1016);
+        }
+    }
+
+    public function id(): string
+    {
+        return $this->id;
+    }
+
+    public function equals(ForumId $userId): bool
+    {
+        return $this->id === $userId->id;
+    }
+}
