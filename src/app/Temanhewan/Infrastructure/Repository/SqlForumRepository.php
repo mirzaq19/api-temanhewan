6e87a71672fbb9659b6ee33bbde4056059cd3059
M. Auliya Mirzaq Romdloni <mirzaqarjap@gmail.com>
2022-05-25 18:20:55 +0700

    feat: add implementation forum repository

---
+++ src/app/Temanhewan/Infrastructure/Repository/SqlForumRepository.php
@@ -0,0 +1,110 @@
+<?php
+
+namespace App\Temanhewan\Infrastructure\Repository;
+
+use App\Temanhewan\Core\Domain\Model\Forum;
+use App\Temanhewan\Core\Domain\Model\ForumId;
+use App\Temanhewan\Core\Domain\Model\UserId;
+use App\Temanhewan\Core\Domain\Repository\ForumRepository;
+use Illuminate\Support\Facades\DB;
+
+class SqlForumRepository implements ForumRepository
+{
+
+    public function byId(ForumId $forumId): ?Forum
+    {
+        $forum_row = DB::table('forums')->where('id', $forumId->id())->first();
+
+        if ($forum_row === null) {
+            return null;
+        }
+
+        return $this->convertRowToForum($forum_row);
+    }
+
+    public function convertRowToForum($forum_row): Forum
+    {
+        return new Forum(
+            new ForumId($forum_row->id),
+            $forum_row->slug,
+            $forum_row->title,
+            $forum_row->subtitle,
+            $forum_row->content,
+            new UserId($forum_row->user_id)
+        );
+    }
+
+    public function saveForumImage(ForumId $forumId, string $filename): void
+    {
+        DB::table('forum_images')->insert([
+            'filename' => $filename,
+            'forum_id' => $forumId->id(),
+            'created_at' => now(),
+            'updated_at' => now(),
+        ]);
+    }
+
+    public function save(Forum $forum): void
+    {
+        DB::table('forums')->insert([
+            'id' => $forum->getId()->id(),
+            'slug' => $forum->getSlug(),
+            'title' => $forum->getTitle(),
+            'subtitle' => $forum->getSubtitle(),
+            'content' => $forum->getContent(),
+            'user_id' => $forum->getUserId()->id(),
+            'created_at' => now(),
+            'updated_at' => now(),
+        ]);
+    }
+
+    public function update(Forum $forum): void
+    {
+        DB::table('forums')
+            ->where('id', $forum->getId()->id())
+            ->update([
+                'slug' => $forum->getSlug(),
+                'title' => $forum->getTitle(),
+                'subtitle' => $forum->getSubtitle(),
+                'content' => $forum->getContent(),
+                'user_id' => $forum->getUserId()->id(),
+                'updated_at' => now(),
+            ]);
+    }
+
+    public function delete(Forum $forum): void
+    {
+        DB::table('forums')->delete($forum->getId()->id());
+    }
+
+    public function listForum(int $offset, int $limit): array
+    {
+        $forums_row = DB::table('forums')
+            ->offset($offset)
+            ->limit($limit)
+            ->get();
+
+        $forums = [];
+
+        foreach ($forums_row as $forum) {
+            $forums[] = $this->convertRowToForum($forum);
+        }
+
+        return $forums;
+    }
+
+    public function listForumByUser(UserId $userId): array
+    {
+        $forums_row = DB::table('forums')
+            ->where('user_id', $userId->id())
+            ->get();
+
+        $forums = [];
+
+        foreach ($forums_row as $forum) {
+            $forums[] = $this->convertRowToForum($forum);
+        }
+
+        return $forums;
+    }
+}
--- src/app/Temanhewan/dependencies.php
+++ src/app/Temanhewan/dependencies.php
@@ -4,11 +4,14 @@ use Illuminate\Contracts\Foundation\Application;
 
 use App\Temanhewan\Core\Domain\Repository\UserRepository;
 use App\Temanhewan\Core\Domain\Repository\PetRepository;
+use App\Temanhewan\Core\Domain\Repository\ForumRepository;
 
 use App\Temanhewan\Infrastructure\Repository\SqlUserRepository;
 use App\Temanhewan\Infrastructure\Repository\SqlPetRepository;
+use App\Temanhewan\Infrastructure\Repository\SqlForumRepository;
 
 /** @var Application $app */
 
 $app->bind(UserRepository::class, SqlUserRepository::class);
 $app->bind(PetRepository::class, SqlPetRepository::class);
+$app->bind(ForumRepository::class, SqlForumRepository::class);
