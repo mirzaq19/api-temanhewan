25f1853d40367ae5ec0f5694caa4e93f0d54005c
M. Auliya Mirzaq Romdloni <mirzaqarjap@gmail.com>
2022-05-31 05:38:46 +0700

    feat: add comments table

---
+++ src/database/migrations/2022_05_31_051026_create_comments_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('comments', function (Blueprint $table) {
+            $table->uuid('id')->primary();
+            $table->text('content');
+            $table->foreignUuid('user_id');
+            $table->foreignUuid('forum_id');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('comments');
+    }
+};
