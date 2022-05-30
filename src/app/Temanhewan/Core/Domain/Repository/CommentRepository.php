5245e0ce7766e9069203ceaca25bc7e1fbb5d953
M. Auliya Mirzaq Romdloni <mirzaqarjap@gmail.com>
2022-05-31 05:56:14 +0700

    feat: add comment repository interface

---
+++ src/app/Temanhewan/Core/Domain/Repository/CommentRepository.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Temanhewan\Core\Domain\Repository;
+
+use App\Temanhewan\Core\Domain\Model\Comment;
+use App\Temanhewan\Core\Domain\Model\CommentId;
+use App\Temanhewan\Core\Domain\Model\ForumId;
+
+interface CommentRepository
+{
+    public function ById(CommentId $id): ?Comment;
+    public function ByForumId(ForumId $forumId): array;
+    public function save(Comment $comment): void;
+    public function remove(Comment $comment): void;
+}
