761d96e3312cafc7784b96d19d09097562b61709
M. Auliya Mirzaq Romdloni <mirzaqarjap@gmail.com>
2022-06-01 18:19:45 +0700

    feat: add consultation repository interface

---
+++ src/app/Temanhewan/Core/Domain/Repository/ConsultationRepository.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Temanhewan\Core\Domain\Repository;
+
+use App\Temanhewan\Core\Domain\Model\Consultation;
+use App\Temanhewan\Core\Domain\Model\ConsultationId;
+use App\Temanhewan\Core\Domain\Model\UserId;
+
+interface ConsultationRepository
+{
+    public function ById(ConsultationId $consultationId): ?Consultation;
+    public function ByDoctorId(UserId $doctorId): array;
+    public function ByCustomerId(UserId $customerId): array;
+    public function save(Consultation $consultation): void;
+    public function update(Consultation $consultation): void;
+    public function delete(Consultation $consultation): void;
+}
